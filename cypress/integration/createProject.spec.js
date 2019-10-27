acb76ae883aa2059a1e1a02938efea5ff56c081e
Walmyr Lima <walmyr@gitlab.com>
2019-10-27 22:28:13 +0100

    Add test that creates a project via API

--- cypress/integration/createProject.spec.js
+++ cypress/integration/createProject.spec.js
@@ -15,4 +15,27 @@ describe("Create Project", () => {
         cy.contains(project.name).should("be.visible");
         cy.contains(project.description).should("be.visible");
     });
+
+    context("Create project via API", () => {
+        const projectName = faker.random.word();
+
+        beforeEach(() => {
+            cy.createAccessToken(faker.random.uuid())
+              .then(accessTokenValue => {
+                cy.request(
+                    "POST",
+                    `/api/v4/projects/?private_token=${accessTokenValue}`,
+                    { name: projectName }
+                );
+              });
+        });
+
+        it("successfully visits the just create project", () => {
+            cy.visit(Cypress.env("user_name") + "/" + projectName);
+
+            cy.get(".qa-project-name")
+              .should("be.visible")
+              .and("contain", projectName);
+        });
+    });
 });
