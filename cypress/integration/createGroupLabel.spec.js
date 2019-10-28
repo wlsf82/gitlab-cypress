c2869dd356707705786faa16918226cfc9f6c9a7
Walmyr Lima <walmyr@gitlab.com>
2019-10-28 19:22:18 +0100

    Update existing test to create group via API

--- cypress/integration/createGroupLabel.spec.js
+++ cypress/integration/createGroupLabel.spec.js
@@ -3,23 +3,26 @@ const faker = require('faker')
 describe('Create Group Label', () => {
   const group = {
     name: faker.random.uuid(),
-    description: faker.random.words(5)
+    path: faker.random.uuid(),
   }
 
   beforeEach(() => {
     cy.login()
-    cy.createPublicGroup(group)
+    cy.createAccessToken(faker.random.uuid())
+      .then(accessTokenValue =>
+        cy.createGroupViaApi(accessTokenValue, group.name, group.path))
+    cy.visit(group.path)
   })
 
   it('successfully creates a group label', () => {
     const label = {
-      group: group.name,
+      group: group.path,
       title: faker.random.word()
     }
 
     cy.createGroupLabel(label)
 
-    cy.url().should('be.equal', `${Cypress.config().baseUrl}groups/${group.name}/-/labels`)
+    cy.url().should('be.equal', `${Cypress.config().baseUrl}groups/${group.path}/-/labels`)
     cy.get('.manage-labels-list').should('contain', label.title)
   })
 })
