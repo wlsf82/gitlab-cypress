b7d3abe852ff339764887df6299cb24256e1bd86
Walmyr Filho <limaesilva@qelp.com>
2019-11-10 00:20:18 +0100

    Add test that closes an issue

---
+++ cypress/integration/closeIssue.spec.js
@@ -0,0 +1,29 @@
+const faker = require('faker')
+
+describe('Close an issue', () => {
+  const projectName = faker.random.uuid()
+  const issueTitle = faker.random.uuid()
+
+  beforeEach(() => {
+    cy.login()
+    cy.createAccessToken(faker.random.uuid())
+      .then(accessTokenValue =>
+        cy.createProjectViaApi(accessTokenValue, projectName)
+          .then(data => {
+            const accessToken = data[0]
+            const projectId = data[1]
+
+            cy.createIssueViaApi(accessToken, projectId, issueTitle)
+              .then(issueIid =>
+                cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${issueIid}`))
+          }))
+  })
+
+  it('successfully', () => {
+    cy.get('.d-none.btn-close').click()
+
+    cy.get('.status-box-issue-closed')
+      .should('be.visible')
+      .and('contain', 'Closed')
+  })
+})
