f0a3ff7a8a66485a64ef9abc4adb6708afc6202c
Walmyr Filho <limaesilva@qelp.com>
2019-11-09 22:20:24 +0100

    Add test to close issue using quick action

---
+++ cypress/integration/closeIssueQuickAction.spec.js
@@ -0,0 +1,35 @@
+const faker = require('faker')
+
+describe('Close an issue using quick action', () => {
+  const projectName = faker.random.uuid()
+  const issueTitle = faker.random.uuid()
+
+  beforeEach(() => {
+    cy.login()
+    cy.createAccessToken(faker.random.uuid())
+      .then(accessTokenValue =>
+        cy.createProjectViaApi(accessTokenValue, projectName)
+          .then(data => {
+            const accessToken = data[0]
+            const projectId = data[1]
+
+            cy.createIssueViaApi(accessToken, projectId, issueTitle)
+              .then(issueIid =>
+                cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${issueIid}`))
+          }))
+  })
+
+  it('successfully', () => {
+    cy.get('.qa-comment-input').type('/close ')
+    cy.get('.qa-comment-button').click()
+
+    cy.contains('Closed this issue')
+      .should('be.visible')
+
+    cy.reload()
+
+    cy.get('.status-box-issue-closed')
+      .should('exist')
+      .and('contain', 'Closed')
+  })
+})
