a1d084cb35103acdf4e591853264536fe6d5ddcc
Walmyr Filho <limaesilva@qelp.com>
2019-11-10 16:20:51 +0100

    Add test that creates a subgroup

---
+++ cypress/integration/subGroup.spec.js
@@ -0,0 +1,20 @@
+const faker = require('faker')
+
+describe('Create a subgroup', () => {
+  const groupName = faker.random.uuid()
+  const groupPath = groupName
+  const subgroup = { name: `sub-group-${faker.random.uuid()}` }
+
+  beforeEach(() => {
+    cy.login()
+    cy.createAccessToken(faker.random.uuid())
+      .then(accessTokenValue =>
+        cy.createGroupViaApi(accessTokenValue, groupName, groupPath)
+          .then(groupId => cy.createSubgroup(groupId, subgroup)))
+  })
+
+  it('successfully', () => {
+    cy.url().should('be.equal', `${Cypress.config().baseUrl}${groupName}/${subgroup.name}`)
+    cy.get('h1').should('contain', subgroup.name)
+  })
+})
