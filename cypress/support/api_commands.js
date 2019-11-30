d55f949198368c2d580832855d817c6b834e845d
Walmyr Filho <limaesilva@qelp.com>
2019-11-30 22:52:10 +0100

    Improve readability of api support commands
    And return the response in the ones where this wasn't being done.

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,36 +1,42 @@
 Cypress.Commands.add('api_createGroup', (accessToken, name, path) => {
-  cy.request(
-    'POST', `/api/v4/groups/?private_token=${accessToken}`, { name, path }
-  ).then(response => response)
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/groups/?private_token=${accessToken}`,
+    body: { name, path }
+  }).then(response => response)
 })
 
 Cypress.Commands.add('api_createProject', (accessToken, name) => {
-  cy.request(
-    'POST', `/api/v4/projects/?private_token=${accessToken}`, { name }
-  ).then(response => response)
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/?private_token=${accessToken}`,
+    body: { name }
+  }).then(response => response)
 })
 
 Cypress.Commands.add('api_createIssue', (accessToken, projectId, title) => {
-  cy.request(
-    'POST', `/api/v4/projects/${projectId}/issues?private_token=${accessToken}`, { title }
-  ).then(response => response)
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/issues?private_token=${accessToken}`,
+    body: { title }
+  }).then(response => response)
 })
 
 Cypress.Commands.add('api_createProjectLabel', (accessToken, projectId, label) => {
-  cy.request(
-    'POST',
-    `/api/v4/projects/${projectId}/labels?private_token=${accessToken}`,
-    {
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/labels?private_token=${accessToken}`,
+    body: {
       name: label.name,
       color: label.color
     }
-  )
+  }).then(response => response)
 })
 
 Cypress.Commands.add('api_createProjectMilestone', (accessToken, projectId, milestone) => {
-  cy.request(
-    'POST',
-    `/api/v4/projects/${projectId}/milestones?private_token=${accessToken}`,
-    { title: milestone.title }
-  )
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/milestones?private_token=${accessToken}`,
+    body: { title: milestone.title }
+  }).then(response => response)
 })
