a8a2d253994b7e7982242580f2ee9d6a9a67ce96
Walmyr Filho <limaesilva@qelp.com>
2020-02-23 17:32:02 +0100

    Add cleanup step on create project api test

--- cypress/integration/api/createProject.spec.js
+++ cypress/integration/api/createProject.spec.js
@@ -1,6 +1,10 @@
 const faker = require('faker')
 
 describe('Create Project', () => {
+  after(() => cy.api_getAllProjects()
+    .then(response => response.body.forEach(project =>
+      cy.api_deleteProject(project.id))))
+
   it('successfully', () => {
     const projectName = faker.random.word()
 
