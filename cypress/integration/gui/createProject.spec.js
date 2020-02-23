4a07742787df002a3516191b91622fbc444f40a9
Walmyr Filho <limaesilva@qelp.com>
2020-02-23 17:35:07 +0100

    Add cleanup step on create project gui test

--- cypress/integration/gui/createProject.spec.js
+++ cypress/integration/gui/createProject.spec.js
@@ -3,6 +3,10 @@ const faker = require('faker')
 describe('Create Project', () => {
   beforeEach(() => cy.gui_login())
 
+  after(() => cy.api_getAllProjects()
+  .then(response => response.body.forEach(project =>
+    cy.api_deleteProject(project.id))))
+
   it('successfully', () => {
     const project = {
       name: faker.random.uuid(),
