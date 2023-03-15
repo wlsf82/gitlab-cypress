0886fedaf49db5624c3f165184bd57805c3761cd
Walmyr <wlsf82@gmail.com>
2023-03-15 02:38:46 +0100

    Mix sub-group creation and search as a single test

--- cypress/e2e/gui/group/subGroup.cy.js
+++ cypress/e2e/gui/group/subGroup.cy.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker/locale/en'
 
-describe('Create a sub-group', () => {
+describe('Sub-group', () => {
   const randomUuid = faker.datatype.uuid()
   const group = {
     name: `group-${randomUuid}`,
@@ -11,6 +11,7 @@ describe('Create a sub-group', () => {
   }
 
   beforeEach(() => {
+    // Arrange
     cy.api_deleteGroups()
     cy.sessionLogin()
     cy.api_createGroup(group)
@@ -18,10 +19,22 @@ describe('Create a sub-group', () => {
       .as('groupBody')
   })
 
-  it('creates a sub-group successfully', function () {
+  it('creates a sub-group and searches for it', function () {
+    // Act
     cy.gui_createSubgroup(this.groupBody.id, group.subgroup)
 
+    // Assert
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}${group.path}/${group.subgroup.name}`)
     cy.contains('h1', group.subgroup.name).should('be.visible')
+
+    // Act
+    cy.visit('dashboard/groups')
+    cy.get('.qa-groups-filter').type(group.subgroup.name)
+
+    // Assert
+    cy.contains(
+      '.qa-groups-list-tree-container',
+      group.subgroup.name
+    ).should('be.visible')
   })
 })
