e036d9aebc5332206f8cd6d534858f92825729ab
Walmyr <wlsf82@gmail.com>
2023-07-06 23:13:17 +0200

    Fix comment

--- cypress/support/commands/session_login.js
+++ cypress/support/commands/session_login.js
@@ -18,7 +18,7 @@ Cypress.Commands.add('sessionLogin', (
   /**
    * @param user string - the id of the session. If the id changes, a new
    * session is created.
-   * @param login function - the function creates the session.
+   * @param login function - the function that creates the session.
    * @param options object - an object to add certain characteristics to the
    * session, such as sharing the cached session across specs (test files),
    * and a way to validate if the session is still valid.
