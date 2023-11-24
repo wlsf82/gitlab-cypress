4d62d4f1d22724c07064564c5bd45c839b353d4d
Walmyr <wlsf82@gmail.com>
2023-11-24 02:28:20 +0100

    Fix and burn flaky test

--- cypress/e2e/gui/project/issueBoard.cy.js
+++ cypress/e2e/gui/project/issueBoard.cy.js
@@ -4,25 +4,31 @@ describe('Issue board', () => {
     cy.sessionLogin()
   })
 
-  it.skip('shows an open issue on the issue board, closes it, and shows it closed', () => {
-    cy.api_createIssue().as('issue')
-    cy.api_getAllProjects()
-      .then(function ({ body }) {
-        const { title, iid } = this.issue.body
-        const { name: projectName } = body[0]
+  Cypress._.times(10, () => {
+    it('shows an open issue on the issue board, closes it, and shows it closed', () => {
+      cy.api_createIssue().as('issue')
+      cy.api_getAllProjects()
+        .then(function ({ body }) {
+          const { title, iid } = this.issue.body
+          const { name: projectName } = body[0]
 
-        cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
+          cy.intercept('PUT', `${Cypress.env('user_name')}/${projectName}/issues/**`)
+            .as('closeIssueRequest')
 
-        cy.contains('[data-board-type="backlog"] [data-qa-selector="board_card"]', title)
-          .should('be.visible')
+          cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
 
-        cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${iid}`)
-        cy.get('.d-none.btn-close').click()
+          cy.contains('[data-board-type="backlog"] [data-qa-selector="board_card"]', title)
+            .should('be.visible')
 
-        cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
+          cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${iid}`)
+          cy.get('.d-none.btn-close').click()
+          cy.wait('@closeIssueRequest')
 
-        cy.contains('[data-board-type="closed"] [data-qa-selector="board_card"]', title)
-          .should('be.visible')
-      })
+          cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
+
+          cy.contains('[data-board-type="closed"] [data-qa-selector="board_card"]', title)
+            .should('be.visible')
+        })
+    })
   })
 })
