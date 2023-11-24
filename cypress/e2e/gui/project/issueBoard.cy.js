d32586523432da59b39e2f8b3c1cee481afc910d
Walmyr <wlsf82@gmail.com>
2023-11-24 02:35:54 +0100

    Remove .times from test
    Now that it has proved to be stable:
    
    - https://github.com/wlsf82/gitlab-cypress/actions/runs/6975809948/
    job/18983441503?pr=40
    - https://cloud.cypress.io/projects/vxwq6z/runs/175/test-results
    
    The fix was a combination of cy.intercept with an alias,
    and a cy.wait for the previously defined alias.

--- cypress/e2e/gui/project/issueBoard.cy.js
+++ cypress/e2e/gui/project/issueBoard.cy.js
@@ -4,31 +4,29 @@ describe('Issue board', () => {
     cy.sessionLogin()
   })
 
-  Cypress._.times(10, () => {
-    it('shows an open issue on the issue board, closes it, and shows it closed', () => {
-      cy.api_createIssue().as('issue')
-      cy.api_getAllProjects()
-        .then(function ({ body }) {
-          const { title, iid } = this.issue.body
-          const { name: projectName } = body[0]
+  it('shows an open issue on the issue board, closes it, and shows it closed', () => {
+    cy.api_createIssue().as('issue')
+    cy.api_getAllProjects()
+      .then(function ({ body }) {
+        const { title, iid } = this.issue.body
+        const { name: projectName } = body[0]
 
-          cy.intercept('PUT', `${Cypress.env('user_name')}/${projectName}/issues/**`)
-            .as('closeIssueRequest')
+        cy.intercept('PUT', `${Cypress.env('user_name')}/${projectName}/issues/**`)
+          .as('closeIssueRequest')
 
-          cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
+        cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
 
-          cy.contains('[data-board-type="backlog"] [data-qa-selector="board_card"]', title)
-            .should('be.visible')
+        cy.contains('[data-board-type="backlog"] [data-qa-selector="board_card"]', title)
+          .should('be.visible')
 
-          cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${iid}`)
-          cy.get('.d-none.btn-close').click()
-          cy.wait('@closeIssueRequest')
+        cy.visit(`${Cypress.env('user_name')}/${projectName}/issues/${iid}`)
+        cy.get('.d-none.btn-close').click()
+        cy.wait('@closeIssueRequest')
 
-          cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
+        cy.visit(`${Cypress.env('user_name')}/${projectName}/-/boards`)
 
-          cy.contains('[data-board-type="closed"] [data-qa-selector="board_card"]', title)
-            .should('be.visible')
-        })
-    })
+        cy.contains('[data-board-type="closed"] [data-qa-selector="board_card"]', title)
+          .should('be.visible')
+      })
   })
 })
